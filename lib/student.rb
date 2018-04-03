3ca941fa7971aace7f5c0258cdb415e65a8c656c
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 18:40:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,7 +46,7 @@ end
 
   def update
     sql = <<-SQL
-    UPDATE students SET name = ?, album = ? WHERE id = ?
+    UPDATE students SET name = ?, grade = ? WHERE id = ?
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade, self.id)
