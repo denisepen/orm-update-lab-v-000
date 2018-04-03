b8313b6c2cab8f37ab1a193296e55b3c69cc0d8c
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 19:02:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ def self.create(name, grade)
 end
 
 def self.new_from_db(row)
-   new_student = self.new(name, grade)
+   new_student = self.new(row[0], row[1], row[2])
 
   # new_student = self.create(name, grade) do |student|
   new_student.id = row[0]
