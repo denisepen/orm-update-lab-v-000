1a4ced8d0e335bcf9bf0226d07edf998a8f0c77b
Denise Pendleton <dpendl1@hotmail.com>
2018-04-03 19:07:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,20 @@ def self.new_from_db(row)
   new_student
 end
 
+def self.find_by_name
+  sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1
+  SQL
+
+  DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+
+
+
   def update
     sql = <<-SQL
     UPDATE students SET name = ?, grade = ? WHERE id = ?
